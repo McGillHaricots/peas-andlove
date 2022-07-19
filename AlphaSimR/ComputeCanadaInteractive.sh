d442e86e76e8f81c2041eb34aad25cde610ccd5e
isabellajchiara <95446790+isabellajchiara@users.noreply.github.com>
2022-07-19 09:53:30 -0400

    Create ComputeCanadaInteractive.sh

---
+++ AlphaSimR/ComputeCanadaInteractive.sh
@@ -0,0 +1,21 @@
+
+##log in to node##
+ssh -Y ich@graham.computecanada.ca
+
+##install any R packages on the login node
+module load gcc/9.3.0 r/4.1.2
+R
+install.packages("package")
+quit()
+##may take up to an hour to load package##
+
+##after quitting R, change to the correct directory##
+cd correctdirectory
+
+##request time and memory for interactive job##
+salloc --time=3:0:0 --mem=300G --account=def-haricots
+
+##once nodes are granted, load R##
+module load gcc/9.3.0 r/4.1.2
+
+####START CODE###
