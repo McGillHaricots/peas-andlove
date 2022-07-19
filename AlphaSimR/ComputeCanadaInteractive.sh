c4537c03a4d08010efa8418efaf4111bfcdc5b5c
isabellajchiara <95446790+isabellajchiara@users.noreply.github.com>
2022-07-19 09:54:24 -0400

    Update ComputeCanadaInteractive.sh

--- AlphaSimR/ComputeCanadaInteractive.sh
+++ AlphaSimR/ComputeCanadaInteractive.sh
@@ -17,5 +17,6 @@ salloc --time=3:0:0 --mem=300G --account=def-haricots
 
 ##once nodes are granted, load R##
 module load gcc/9.3.0 r/4.1.2
+R
 
-####START CODE###
+####you are now in an interactive R session, start running code###
