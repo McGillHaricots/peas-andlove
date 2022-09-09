86d7fb9ded0e207d9e9a7928cb94b2060f4352e9
isabellajchiara <95446790+isabellajchiara@users.noreply.github.com>
2022-09-09 11:23:32 -0400

    Update RunReps.R

--- AlphaSimR/Extras/RunReps.R
+++ AlphaSimR/Extras/RunReps.R
@@ -1,8 +1,8 @@
-nReps = 25
+nReps = 5
 
 for(rep in 1:nReps){
   # pull in desired script # 
-  source(rrBLUP_random.R")
-  output = (list(F1, F2, F3, F4, F5, PYT, AYT, Variety, gv, cor)
-  write.csv(output, "model_trn_poptype_trait.csv")
+  source("rrBLUP_random.R")
+  output = list(F1, F2, F3, F4, F5, PYT, AYT, Variety, gv, cor)
+  write.csv(output, "rrBLUP_random_SR_yield.csv")
   }
