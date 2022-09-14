6e52cdf9d92671a75b5968941bfac75b91fb5daa
isabellajchiara <95446790+isabellajchiara@users.noreply.github.com>
2022-09-14 13:59:03 -0400

    Update GetResults_RunReps.R

--- AlphaSimR/GetResults_RunReps.R
+++ AlphaSimR/GetResults_RunReps.R
@@ -30,16 +30,16 @@ corResults <- matrix(nrow=nModels, ncol=nReps)
 
 i = 1 
 repeat{
-  source("rrBLUP_random.R") ##Source the script for the scenario you would like to run##
-  F1results[,i] = F1gv
-  F2results[,i] = F2gv
-  F3results[,i] = F3gv
-  F4results[,i] = F4gv
-  F5results[,i] = F5gv
-  PYTresults[,i] = PYTgv
-  AYTresults[,i] = AYTgv
-  Varietyresults[,i] = Varietygv
-  corResults[,i] = corMat
+  source("rrBLUP_stratClustersSRNM.R") ##Source the script for the scenario you would like to run##
+  F1results[,i] <- F1gv ##fills i column with F1gv results etc
+  F2results[,i] <- F2gv
+  F3results[,i] <- F3gv
+  F4results[,i] <- F4gv
+  F5results[,i] <- F5gv
+  PYTresults[,i] <- PYTgv
+  AYTresults[,i] <- AYTgv
+  Varietyresults[,i] <- Varietygv
+  corResults[,i] <- corMat
   
   i <- i + 1
   
@@ -75,8 +75,8 @@ repeat{
   
   
   ##write files
-  write.csv(allResults, "BLUP_Random_Allgvs_SR_Yield.csv")
-  write.csv(corResults, "BLUP_Random_Correlation_SR_Yield.csv")
+  write.csv(allResults, "BLUP_stratClusters_Allgvs_SRNM_Yield.csv")
+  write.csv(corResults, "BLUP_stratClusters_Correlation_SRNM_Yield.csv")
   
            
   
