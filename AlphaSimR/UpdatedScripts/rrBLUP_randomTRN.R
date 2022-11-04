ff9c8dc133e59e7a3f83982a2c6d0093cb584b09
isabellajchiara <95446790+isabellajchiara@users.noreply.github.com>
2022-11-04 09:09:08 -0400

    Create rrBLUP_randomTRN.R

---
+++ AlphaSimR/UpdatedScripts/rrBLUP_randomTRN.R
@@ -0,0 +1,14 @@
+
+M <- pullSegSiteGeno(TrainingPop)
+y <- pheno(TrainingPop)
+
+trainIndex <- as.matrix(sample(1:nInd(TrainingPop), 0.8*(nrow(M)))) 
+
+phenoTrain <- y[trainIndex,]
+genoTrain <- M[trainIndex,]
+
+BV <- phenoTrain
+
+EBVans <-mixed.solve(BV, Z=genoTrain, K=NULL, SE=FALSE, return.Hinv=FALSE)
+markerEffects <- EBVans$u
+markerEffects <- as.vector(markerEffects)
